9149e6ed7f267690a1800535942bc0d3c74ea2f4
Gord Pavel <pvgord@icloud.com>
2021-03-30 10:34:20 +0400

    fix: configure semver plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
+import io.wusa.GitService.Companion.lastTag
+import io.wusa.RegexResolver.Companion.findMatchingRegex
+import io.wusa.SemanticVersionFactory
+import io.wusa.TagType.LIGHTWEIGHT
 import io.wusa.extension.SemverGitPluginExtension
+import io.wusa.extension.SemverGitPluginExtension.Companion.DEFAULT_INCREMENTER
+import io.wusa.incrementer.VersionIncrementer.Companion.getVersionIncrementerByName
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -7,7 +13,49 @@ plugins {
     kotlin("jvm") version "1.4.30" apply false
     kotlin("plugin.spring") version "1.4.30" apply false
     id("com.bmuschko.docker-spring-boot-application") version "6.7.0" apply false
-    id("semver")
+    id("io.wusa.semver-git-plugin") version ("2.3.7")
+}
+
+allprojects {
+
+    apply {
+        plugin("io.wusa.semver-git-plugin")
+    }
+
+    semver {
+        snapshotSuffix = ""
+        dirtyMarker = ""
+        initialVersion = "0.1.0"
+        tagType = LIGHTWEIGHT
+        branches {
+            branch {
+                regex = "master"
+                incrementer = "CONVENTIONAL_COMMITS_INCREMENTER"
+                formatter = Transformer {
+                    "${semver.info.version.major}.${semver.info.version.minor}.${semver.info.version.patch}"
+                }
+            }
+            branch {
+                regex = ".+"
+                incrementer = "NO_VERSION_INCREMENTER"
+                formatter = Transformer {
+                    "${semver.info.version.major}.${semver.info.version.minor}.${semver.info.version.patch}-${semver.info.branch.name}.build.${semver.info.count}"
+                }
+            }
+        }
+    }
+
+    tasks.register("incrementVersion") {
+        val semanticVersionFactory = SemanticVersionFactory()
+        val semverGitPluginExtension = project.extensions.getByType(SemverGitPluginExtension::class)
+        val tagPrefix = semverGitPluginExtension.tagPrefix
+        val lastTag = lastTag(project, tagPrefix, tagType = semverGitPluginExtension.tagType)
+        val incrementer = findMatchingRegex(
+            semverGitPluginExtension.branches,
+            semverGitPluginExtension.info.branch.name
+        )?.let { getVersionIncrementerByName(it.incrementer) } ?: getVersionIncrementerByName(DEFAULT_INCREMENTER)
+        incrementer.increment(semanticVersionFactory.createFromString(lastTag.substring(tagPrefix.length)), project)
+    }
 }
 
 subprojects {
@@ -16,7 +64,6 @@ subprojects {
     }
 
     apply {
-        plugin("semver")
         plugin("io.spring.dependency-management")
     }
 
@@ -26,6 +73,4 @@ subprojects {
             jvmTarget = "11"
         }
     }
-
-    version = (extensions["semver"] as SemverGitPluginExtension).info
 }
